be37793c2f9955150bd7051395c2c3f2268a1dec
Hisham PP <mohammedhisham@kiebot.com>
2024-08-24 14:33:03 +0530

    Hisham | Button type fix

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -1,13 +1,7 @@
 import { ButtonHTMLAttributes, ReactNode } from "react";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  children?: ReactNode;
   color?: string;
   bgColor?: string;
   fontSize?: string;
-  onClick?: () => void;
-  type?: "button" | "submit" | "reset";
-  disabled?: boolean;
-  className?: string;
-  label?: string;
 }
