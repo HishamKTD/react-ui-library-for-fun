f0c90f4f8182e8358cc6cb19a6585c5d6eb4018e
Hisham PP <mohammedhisham@kiebot.com>
2024-08-24 14:36:45 +0530

    Hisham | button type issue fix v2

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes, ReactNode } from "react";
+import { ButtonHTMLAttributes } from "react";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   color?: string;
